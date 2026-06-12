c593a667962dc5244bcad39dbaba4206fd844783
ClarenzeJames <38415063+ClarenzeJames@users.noreply.github.com>
2026-06-12 23:44:54 +0800

    Finished Phase 8

--- src/main/java/com/example/inventory_management/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/example/inventory_management/service/Impl/ProductServiceImpl.java
@@ -2,24 +2,42 @@ package com.example.inventory_management.service.Impl;
 
 import com.example.inventory_management.model.Product;
 import com.example.inventory_management.repository.ProductRepository;
+import com.example.inventory_management.service.ProductService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
     private final ProductRepository repository;
 
-//    @Autowired
-//    public ProductServiceImpl(ProductRepository repository) {
-//        this.repository = repository;
-//    }
+    @Override
+    public Product createProduct(Product product) {
+        return null;
+    }
 
     @Override
-    public createProduct(Product product) {
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Product updateProduct(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
 
     }
 }
