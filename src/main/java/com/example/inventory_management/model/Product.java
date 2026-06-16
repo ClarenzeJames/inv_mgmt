dbf050dd0f4ab9b9cb5c762136810667b06216d2
ClarenzeJames <38415063+ClarenzeJames@users.noreply.github.com>
2026-06-16 11:17:19 +0800

    Completed Input validation

--- src/main/java/com/example/inventory_management/model/Product.java
+++ src/main/java/com/example/inventory_management/model/Product.java
@@ -2,6 +2,8 @@ package com.example.inventory_management.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,8 +17,10 @@ public class Product {
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank(message = "Name is required")
     private String name;
 
     @Column(nullable = false)
+    @Min(value = 0, message="Price has to be more than 0")
     private Float price;
 }
