65d8be30898d068eaefe6c8f5bd9fe98e48fbd24
ClarenzeJames <38415063+ClarenzeJames@users.noreply.github.com>
2026-06-16 14:52:00 +0800

    JUnit testing donedt

--- src/test/java/com/example/inventory_management/InventoryManagementApplicationTests.java
+++ src/test/java/com/example/inventory_management/InventoryManagementApplicationTests.java
@@ -1,10 +1,30 @@
 package com.example.inventory_management;
 
+import com.example.inventory_management.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class InventoryManagementApplicationTests {
+
+    @Autowired
+    private ProductService service;
+
+    @Test
+    void shouldReturnName(){
+        String name = String.valueOf(service.getProductById(1L).getName());;
+        assertEquals("Laptop ", name);
+    }
+
+    @Test
+    void shouldBeMoreThanZero(){
+        Float price = service.getProductById(1L).getPrice();
+        assertTrue(price > 0);
+    }
 
     @Test
     void contextLoads() {
